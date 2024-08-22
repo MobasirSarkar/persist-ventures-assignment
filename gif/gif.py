4cc5366b64069c31c6cba736a381c50f2fb16550
MobasirSarkar <mobasirgithubonly@gmail.com>
2024-08-22 23:54:52 +0530

    automation script

--- gif/gif.py
+++ gif/gif.py
@@ -1,12 +1,11 @@
-from selenium import webdriver
 import time
 import pyperclip
 from selenium.webdriver.common.by import By
-from concurrent.futures import ThreadPoolExecutor
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 class Gif:
-
     def __init__(self, gif_id, driver):
         self.gif_id = gif_id
         self.driver = driver
@@ -14,36 +13,30 @@ class Gif:
     def search_gif(self):
         driver = self.driver
         driver.get(f"https://tenor.com/view/{self.gif_id}")
-        time.sleep(2)
-        with ThreadPoolExecutor(max_workers=50) as executor:
-            futures = [executor.submit(self.click_and_copy) for _ in range(100)]
-            for future in futures:
-                try:
-                    future.result()
-                except Exception as e:
-                    print(f"An error has occured{e}")
-        time.sleep(2)
+        time.sleep(1)
+        for _ in range(100):
+            try:
+                driver.execute_script("window.scrollBy(0,400)")
+                element = driver.find_element(
+                    By.XPATH,
+                    "/html/body/div/div/div[3]/div[1]/div[2]/div[3]/div[1]/input",
+                )
+                driver.execute_script(
+                    "window.scrollBy(0,arguments[0].getBoundingClientRect().top - 100)",
+                    element,
+                )
+                time.sleep(2)
+                element.click()
+                time.sleep(2)
 
-    def click_and_copy(self):
-        driver = self.driver
-        try:
-            element = driver.find_element(
-                By.XPATH,
-                "/html/body/div/div/div[3]/div[1]/div[2]/div[3]/div[1]/input",
-            )
+                copied_link = pyperclip.paste()
 
-            driver.execute_script("arguments[0].scrollIntoView(true);", element)
-            time.sleep(2)
-            element.click()
-            time.sleep(2)
+                driver.execute_script("window.open('');")
+                driver.switch_to.window(driver.window_handles[-1])
+                driver.get(copied_link)
+                driver.close()
+                driver.switch_to.window(driver.window_handles[0])
 
-            copied_link = pyperclip.paste()
-
-            driver.execute_script("window.open('');")
-            driver.switch_to.window(driver.window_handles[-1])
-            driver.get(copied_link)
-
-            driver.close()
-            driver.switch_to.window(driver.window_handles[0])
-        except Exception as e:
-            print(f"An Error has occured : {e}")
+            except Exception as e:
+                print(f"An error occurred: {e}")
+                break  # Optional: exit the loop on error
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from gif.gif import Gif
+from concurrent.futures import ThreadPoolExecutor
 
 
 def process_gif(gif_id):
@@ -11,5 +12,10 @@ def process_gif(gif_id):
 
 
 if __name__ == "__main__":
-    gif_id = ["one-piece-monkey-d-luffy-straw-hat-luffy-lol-gif-16566941"]
-    process_gif(gif_id)
+    gif_id = ["luffy-gear-5-luffy-monkey-d-luffy-one-piece-wano-gif-241855050869579563"]
+
+    with ThreadPoolExecutor(max_workers=10) as executor:
+        futures = [executor.submit(process_gif, gif_id) for _ in range(10)]
+
+    for future in futures:
+        future.result()
